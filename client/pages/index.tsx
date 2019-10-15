f82e7f20c4657d474f9eb9dc0e2efd78feee0abc
EddyVinck <eddyvinck95@gmail.com>
2019-10-15 21:53:33 +0200

    add the mailchimp form

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -5,7 +5,6 @@ import Nav from "../components/nav";
 import { getTalkList } from "../utils/data-formatting/getTalkList";
 import { Form, Field, Formik } from "formik";
 import findIndex from "lodash-es/findIndex";
-// import useLocalStorage from "react-use-localstorage";
 import * as gtag from "../utils/analytics/gtag";
 
 const TalkList = styled.ol({
@@ -16,13 +15,14 @@ const TalkList = styled.ol({
   li: {
     a: {
       display: "block",
+      position: "relative",
       color: "#000",
       textDecoration: "none",
       padding: "1rem 0.5rem",
       background: "white",
       borderBottom: "2px solid lightgray",
-      "&: visited": {
-        color: "#aaa"
+      "&:visited": {
+        color: "#666"
       },
       "&:hover": {
         color: "blue"
@@ -142,25 +142,38 @@ const Home = () => {
 
         <section>
           <h2>Subscribe to the ConfTalks newsletter</h2>
-          <Formik
-            initialValues={{ email: "" }}
-            onSubmit={() => {
-              // gtag.event({
-              //   action: 'submit_form',
-              //   category: 'Newsletter',
-              //   label: email
-              // })
+          <div
+            dangerouslySetInnerHTML={{
+              __html: `
+            <!-- Begin Mailchimp Signup Form -->
+            <link href="//cdn-images.mailchimp.com/embedcode/horizontal-slim-10_7.css" rel="stylesheet" type="text/css">
+            <style type="text/css">
+              #mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; width:100%;}
+              /* Add your own Mailchimp form style overrides in your site stylesheet or in this style block.
+                 We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+            </style>
+            <style type="text/css">
+              #mc-embedded-subscribe-form input[type=checkbox]{display: inline; width: auto;margin-right: 10px;}
+              #mergeRow-gdpr {margin-top: 20px;}
+              #mergeRow-gdpr fieldset label {font-weight: normal;}
+              #mc-embedded-subscribe-form .mc_fieldset{border:none;min-height: 0px;padding-bottom:0px;}
+            </style>
+            <div id="mc_embed_signup">
+            <form action="https://gmail.us20.list-manage.com/subscribe/post?u=8a83058b9ef8a5d4d9076f258&amp;id=7fcdb807f6" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+                <div id="mc_embed_signup_scroll">
+              <label for="mce-EMAIL">Subscribe</label>
+              <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
+                <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+                <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_8a83058b9ef8a5d4d9076f258_7fcdb807f6" tabindex="-1" value=""></div>
+                <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+                </div>
+            </form>
+            </div>
+            
+            <!--End mc_embed_signup-->
+            `
             }}
-          >
-            {({ isSubmitting }) => (
-              <Form>
-                <Field type="email" name="email" />
-                <button type="submit" disabled={isSubmitting}>
-                  Submit
-                </button>
-              </Form>
-            )}
-          </Formik>
+          />
         </section>
 
         <aside>
@@ -173,7 +186,17 @@ const Home = () => {
             className += talk.bookmarked ? " is-bookmarked" : "";
             return (
               <li key={talk.id} className={className}>
-                <a href={talk.video_url} target="blank" rel="noopener">
+                <a
+                  href={talk.video_url || "#"}
+                  target="blank"
+                  rel="noopener"
+                  onClick={event => {
+                    if (!talk.video_url) {
+                      event.preventDefault();
+                      alert("There is no video submitted for this talk");
+                    }
+                  }}
+                >
                   <h2>{talk.main_title}</h2>
                   <div className="horizontal-list-wrapper">
                     <p className="horizontal-list-label">By:</p>
